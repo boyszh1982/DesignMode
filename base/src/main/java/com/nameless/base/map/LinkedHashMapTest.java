a4b4d58095a1568dc0cbfa0c022fa626a8aae848
stanley <boyszh1982@163.com>
2018-12-04 15:08:06 +0800

    add linkedHashMapTest

--- base/src/main/java/com/nameless/base/map/LinkedHashMapTest.java
+++ base/src/main/java/com/nameless/base/map/LinkedHashMapTest.java
@@ -5,6 +5,14 @@ import java.util.*;
 public class LinkedHashMapTest {
 
 	public static void main(String[] args) {
+		LinkedHashMap<String,Integer> scores = new LinkedHashMap<String,Integer>(16,0.75f,false);
+		scores.put("语文", 80);
+		scores.put("英文", 82);
+		scores.put("数学", 76);
+		System.out.println(scores.get("英文"));
+	}
+
+	public static void main2(String[] args) {
 
 		/*
 		LinkedHashMap<String,Integer> scores = new LinkedHashMap<>();
@@ -38,17 +46,44 @@ public class LinkedHashMapTest {
 			System.out.println(entry.toString());
 		}
 
+
+		HashMap<Integer,Integer> cntmap = new HashMap<Integer,Integer>();
 		// speed test
-		LinkedHashMap<String,Integer> speedmap = new LinkedHashMap<String,Integer>(16,0.75f,true);
+		LinkedHashMap<String,Integer> speedmap = new LinkedHashMap<String,Integer>(16,0.75f,false);
 		//HashMap<String,Integer> speedmap = new HashMap<String,Integer>();
-		for(int i=0;i<10000000;i++){
+		String key = null;
+		for(int i=0;i<10;i++){
+			key = "S_"+i;
 			speedmap.put("S_"+i,i);
+
+			if( cntmap.containsKey(hash(key)) ) {
+				cntmap.put(hash(key),cntmap.get(hash(key))+1);
+			}
+			else{
+				cntmap.put(hash(key),1);
+			}
+			//System.out.println(hash(hash(key)));
 		}
 
 		System.out.println(System.currentTimeMillis());
 		speedmap.get("S_1");
 		System.out.println(System.currentTimeMillis());
-		speedmap.get("S_9000000");
-		System.out.println(System.currentTimeMillis());
+		speedmap.get("S_10");
+
+		//System.out.println(cntmap.toString());
+		/*
+		Iterator<Map.Entry<Integer,Integer>> itercntmap = cntmap.entrySet().iterator();
+		while (iter.hasNext()){
+			Map.Entry<Integer,Integer> entry = itercntmap.next();
+			if(entry.getValue() > 1)
+				System.out.println(entry.toString());
+		}
+		*/
+
+	}
+
+	static final int hash(Object key) {
+		int h;
+		return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
 	}
 }
